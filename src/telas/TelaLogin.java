c149ddacc90c8216ad871066df0a8c9d66f5b69d
pedrohgceolin <57969215+pedrohgceolin@users.noreply.github.com>
2022-07-27 17:00:37 -0300

    Criação de telas
    Criação de algumas telas em java

---
+++ src/telas/TelaLogin.java
@@ -0,0 +1,15 @@
+package telas;
+
+import javax.swing.*;
+
+public class TelaLogin {
+    private JTextField textField1;
+
+    public static void main(String[] args) {
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
